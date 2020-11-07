66da4267de66fab2d5222b59820c616f6d9cbb18
minamoto xu <minamoto.xu@outlook.com>
2020-11-06 22:55:56 -0500

    fix v2 run sh

--- buildfiles/bin/runv2ray.sh
+++ buildfiles/bin/runv2ray.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-pkill -f trojan
-pkill -f v2ray
+start-stop-daemon --stop -p /run/v2.pid
 
 # service privoxy restart
 
 # start-stop-daemon --start --name trojanc --background --exec /root/bin/trojan -- -c /root/bin/trojan.client
-start-stop-daemon --start --name v2rayc --background --exec /root/bin/v2ray -- -config=/root/bin/v2client.json
+start-stop-daemon --start -m -p /run/v2.pid --background --exec /root/bin/v2ray -- -config=/root/bin/v2client.json
 
 # /bin/bash
